49e6e957efe8773a70af33626b991fab6d1d0bc4
Broken Pipe <navigaid@gmail.com>
2026-06-14 01:22:26 +0800

    fix: replace bufio.Scanner with io.Copy in busybox cat
    Scanner + Println breaks binary files (64KB line limit) and adds trailing
    newlines. io.Copy preserves exact byte content and handles arbitrary data.

--- busybox/cat/run.go
+++ busybox/cat/run.go
@@ -1,7 +1,6 @@
 package cat
 
 import (
-	"bufio"
 	"fmt"
 	"io"
 	"os"
@@ -10,7 +9,8 @@ import (
 func Run(args []string) error {
 	files := args[1:]
 	if len(files) == 0 {
-		return cat(os.Stdin)
+		_, err := io.Copy(os.Stdout, os.Stdin)
+		return err
 	}
 	for _, path := range files {
 		f, err := os.Open(path)
@@ -18,19 +18,11 @@ func Run(args []string) error {
 			fmt.Fprintf(os.Stderr, "cat: %v\n", err)
 			continue
 		}
-		err = cat(f)
+		_, err = io.Copy(os.Stdout, f)
 		f.Close()
 		if err != nil {
 			return err
 		}
 	}
 	return nil
-}
-
-func cat(r io.Reader) error {
-	sc := bufio.NewScanner(r)
-	for sc.Scan() {
-		fmt.Println(sc.Text())
-	}
-	return sc.Err()
 }
